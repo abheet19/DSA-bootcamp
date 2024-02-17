3c05e1082c271adfde6f79285601cf337da8ad5d
abheet19 <abheet19@gmail.com>
2024-02-17 18:11:04 +0530

    initial commit test

---
+++ LinkedList/Basics.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define fast ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+
+void solve(){
+	
+}
+
+
+
+
+signed main(){
+
+    fast;
+    solve();
+    
+    return 0;
+}
