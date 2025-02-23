afdbacbab4408a542b871ecbce33e3c8806a0b54
Abheet Singh Isher <abheet19@gmail.com>
2025-02-23 22:21:13 +0530

    added custom polyfills for Array methods

---
+++ Interview Questions/Frontend/array_polyfills.js
@@ -0,0 +1,65 @@
+//Polyfills are pieces of code that provide modern functionality on older browsers or environments that do not natively support it.
+//They essentially "fill in" the gaps by mimicking the behavior of modern JavaScript features.
+
+Array.prototype.myMap = function (cb) {
+  if (typeof cb !== "function") {
+    throw new TypeError(cb + "is not a function");
+  }
+  const result = [];
+
+  for (let i = 0; i < this.length; i++) {
+    result.push(cb(this[i], i, this));
+  }
+  return result;
+};
+const arr = [1, 2, 3, 4, 5];
+const result = arr.myMap((x) => x * 2);
+console.log(result); //[2,4,6,8,10]
+
+Array.prototype.myFilter = function (cb) {
+  if (typeof cb !== "function") {
+    throw new TypeError(cb + "is not a function");
+  }
+  const result = [];
+  for (let i = 0; i < this.length; i++) {
+    if ((cb[this], i, this)) {
+      result.push(this[i]);
+    }
+  }
+  return result;
+};
+
+const evenNumbers = arr.myFilter((x) => x % 2 === 0);
+console.log(evenNumbers); //[2,4]
+
+Array.prototype.myReduce = function (cb, intialValue) {
+  if (typeof cb !== "function") {
+    throw new TypeError(cb + "is not a function");
+  }
+  let acc = initialValue !== undedined ? initialValue : this[0];
+  const startIndex = initialValue !== undefined ? 0 : 1;
+
+  for (let i = 0; i < this.length; i++) {
+    acc = cb((acc, this[i], i, this));
+  }
+  return acc;
+};
+const sum = arr.myReduce((acc, current) => acc + current, 0);
+console.log(sum); //15
+const product = arr.myReduce((acc, current) => acc * current, 1);
+console.log(product); //120
+
+Array.prototype.myEvery = function (cb) {
+  if (typeof cb !== "function") {
+    throw new TypeError(cb + "is not a function");
+  }
+  for (let i = 0; i < this.length; i++) {
+    if (!cb(this[i], i, this)) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const areAllPositive = arr.myEvery((x) => x > 0);
+console.log(areAllPositive); //true
