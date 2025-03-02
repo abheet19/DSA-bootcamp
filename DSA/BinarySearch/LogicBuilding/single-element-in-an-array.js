cd5062dbdeb17f4dc03cd0f4c73e4bd08773948b
Abheet Singh Isher <abheet19@gmail.com>
2025-03-02 16:24:00 +0530

    added single element optimal approach

--- DSA/BinarySearch/LogicBuilding/single-element-in-an-array.js
+++ DSA/BinarySearch/LogicBuilding/single-element-in-an-array.js
@@ -1,5 +1,5 @@
 class Solution {
-    //Time Complexity O(n)
+  //Time Complexity O(n)
   singleNonDuplicateBruteForce(nums) {
     const n = nums.length;
     if (n === 1) {
@@ -32,9 +32,44 @@ class Solution {
     });
     return ans;
   }
+  //[1 1 2 2 3 3 4 5 5 6 6]
+  // 0 1 2 3 4 5 6 7 8 9 10
+  singleNonDuplicateOptimal(nums) {
+    const n = nums.length ;
+
+    if (n === 1) {
+      return nums[0];
+    }
+    if (nums[0] !== nums[1]) {
+      return nums[0];
+    }
+    if (nums[n - 1] !== nums[n - 2]) {
+      return nums[n - 1];
+    }
+    let low = 1;
+    let high = n - 2;
+    while (low <= high) {
+      let mid = low + Math.floor((high - low) / 2);
+
+      if (nums[mid] !== nums[mid - 1] && nums[mid] !== nums[mid + 1]) {
+        return nums[mid];
+      }
+      if (
+        (mid % 2 === 1 && nums[mid] === nums[mid - 1]) || //We are standing on odd index check prev element , if same eliminate left half
+        (mid % 2 === 0 && nums[mid] === nums[mid + 1]) //or standing on even index , check next element , if same eliminate left half
+      ) {
+        low = mid + 1;
+      } else {
+        high = mid - 1;
+      }
+    }
+    return -1;
+  }
 }
 
 solution = new Solution();
 console.log(solution.singleNonDuplicateBruteForce([1, 1, 2, 3, 3, 4, 4, 8, 8])); //2
 
 console.log(solution.singleNonDuplicateUsingXOR([1, 1, 2, 3, 3, 4, 4, 8, 8])); //2
+
+console.log(solution.singleNonDuplicateOptimal([1, 1, 2, 3, 3, 4, 4, 8, 8])); //2
